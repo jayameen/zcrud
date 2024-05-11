13b07bc8c1f7431c9b2c03b264b47e9b8c160e4e
madankn <madan.kn@gmail.com>
2024-05-11 17:52:32 +0530

    Initial Template Commit

---
+++ src/main/java/com/jayameen/zcrud/config/ApplicationBeanConfigurations.java
@@ -0,0 +1,28 @@
+package com.jayameen.zcrud.config;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.azure.storage.blob.BlobContainerClient;
+import com.azure.storage.blob.BlobServiceClient;
+import com.azure.storage.blob.BlobServiceClientBuilder;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.ByteArrayInputStream;
+import java.util.Base64;
+
+/**
+ * @author Madan KN
+ */
+
+@Configuration
+public class ApplicationBeanConfigurations {
+
+}
